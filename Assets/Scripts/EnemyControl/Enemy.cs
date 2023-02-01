d84daa54230d1d43e2a3bde55edbbbe44f660bbd
Laia <laia2000rc@gmail.com>
2023-02-02 00:56:10 +0100

    Wave system and enemy offset movement

--- Assets/Scripts/EnemyControl/Enemy.cs
+++ Assets/Scripts/EnemyControl/Enemy.cs
@@ -8,10 +8,12 @@ public class Enemy : MonoBehaviour
     public int maxHealth;
     public float speed;
     public string type; //flying, etc
-    public Transform[] waypoints;
+    public List<Vector3> waypoints = new List<Vector3>();
 
     public int waypointIndex = 0;
     public int currentHealth;
+
+    public static float TARGET_OFFSET = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -27,9 +29,14 @@ public class Enemy : MonoBehaviour
 
     void initialize()
     {
+        int index = 0;
         //get waypoints from GameManager
-        waypoints = GameManager.Instance.getWaypoints(type);
-        transform.position = waypoints[0].position;
+        foreach (Transform waypoint in GameManager.Instance.getWaypoints(type))
+        {
+            index++;
+            Vector3 offsetWaypoint = getRandomPoint(waypoint.position);
+            waypoints.Add(offsetWaypoint);
+        }
         currentHealth = maxHealth;
     }
 
@@ -37,13 +44,14 @@ public class Enemy : MonoBehaviour
     {
         //Put z to 0
         transform.position = new Vector3(transform.position.x, transform.position.y, 0);
-        waypoints[waypointIndex].transform.position = new Vector3(waypoints[waypointIndex].transform.position.x, waypoints[waypointIndex].transform.position.y, 0);
+        waypoints[waypointIndex] = new Vector3(waypoints[waypointIndex].x, waypoints[waypointIndex].y, 0);
 
-        transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex], speed * Time.deltaTime);
+        //getRandomPoint(waypoints[waypointIndex].transform.position)
 
-        if(Vector3.Distance(transform.position, waypoints[waypointIndex].transform.position) < 0.5) waypointIndex += 1;
+        if (Vector3.Distance(transform.position, waypoints[waypointIndex]) < 0.5) waypointIndex += 1;
 
-        if (waypointIndex == waypoints.Length)
+        if (waypointIndex == waypoints.Count)
         {
             //Remove one life from the player
             GameManager.Instance.removePlayerHP();
@@ -64,5 +72,10 @@ public class Enemy : MonoBehaviour
     public void die()
     {
         gameObject.SetActive(false);
+    }
+
+    private Vector3 getRandomPoint(Vector3 center)
+    {
+        return center + new Vector3((Random.value - 0.5f) * TARGET_OFFSET, (Random.value - 0.5f) * TARGET_OFFSET, 0);
     }
 }
--- Assets/Scripts/EnemyControl/EnemySpawn.cs
+++ Assets/Scripts/EnemyControl/EnemySpawn.cs
@@ -5,19 +5,53 @@ using UnityEngine;
 [System.Serializable]
 public class Pool
 {
+    public string enemyId;
     public GameObject objectToPool;
     public int amountToPool;
-    public float spawnRate;
     public List<GameObject> pooledObjects;
 }
+
+public class EnemyWaveState
+{
+    public int totalEnemies;
+    public int numSpawned;
+    public bool waveActive;
+    public float counter;
+    public string enemyId;
+    public float spawnRate;
+    public int amount;
+    public float delay;
+    public bool completed;
+
+    public EnemyWaveState(int totalEnemies, int numSpawned, bool waveActive, float counter, string enemyId, float spawnRate, int amount, float delay, bool completed)
+    {
+        this.totalEnemies = totalEnemies;
+        this.numSpawned = numSpawned;
+        this.waveActive = waveActive;
+        this.counter = counter;
+        this.enemyId = enemyId;
+        this.spawnRate = spawnRate;
+        this.amount = amount;
+        this.delay = delay;
+        this.completed = completed;
+    }
+}
+
+
 
 [System.Serializable]
 public class EnemySpawn : MonoBehaviour
 {
-    public Pool[] pools;
+    public List<Pool> pools;
+    public string id;
+    public float area;
 
-    private float[] counters;
+    public static int AREA_RANGE = 5;
 
+    private bool isActive = false;
+    private List<EnemyWave> enemyWaves = new List<EnemyWave>();
+    private List<EnemyWaveState> enemyWaveStates = new List<EnemyWaveState>();
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -32,33 +66,61 @@ public class EnemySpawn : MonoBehaviour
                 pool.pooledObjects.Add(aux);
             }
         }
-
-        //Initialize counters
-        counters = new float[pools.Length];
     }
 
     // Update is called once per frame
     void Update()
-    {
-        for (int i = 0; i < pools.Length; i++)
+    { 
+        if (isActive) //and not paused
         {
-            counters[i] += Time.deltaTime;
-
-            if (counters[i] >= pools[i].spawnRate)
+            foreach (EnemyWaveState enemyWaveState in enemyWaveStates)
             {
-                spawnEnemy(i);
-                counters[i] = 0;
+                if (!enemyWaveState.completed)
+                {
+                    if (!enemyWaveState.waveActive)
+                    {
+                        enemyWaveState.counter += Time.deltaTime;
+                        if (enemyWaveState.counter >= enemyWaveState.delay && !enemyWaveState.waveActive)
+                        {
+                            enemyWaveState.waveActive = true;
+                            enemyWaveState.counter = enemyWaveState.spawnRate;
+                        }
+                    }
+                    else
+                    {
+                        enemyWaveState.counter += Time.deltaTime;
+                        if (enemyWaveState.counter >= enemyWaveState.spawnRate)
+                        {
+                            spawnEnemy(getPoolIndex(enemyWaveState.enemyId), enemyWaveState);
+                            enemyWaveState.counter = 0;
+                        }
+                    }
+                }
             }
         }
     }
 
-    public void spawnEnemy(int poolIndex)
+    public void spawnEnemy(int poolIndex, EnemyWaveState enemyWaveState)
     {
-        GameObject enemy = getPooledObject(poolIndex);
-        enemy.transform.position = transform.position;
-        enemy.GetComponent<Enemy>().waypointIndex = 0;
-        enemy.GetComponent<Enemy>().currentHealth = enemy.GetComponent<Enemy>().maxHealth;
-        enemy.SetActive(true);
+        if(poolIndex >= 0)
+        {
+            for (int i = 0; i < enemyWaveState.amount; i++)
+            {
+                if(enemyWaveState.numSpawned < enemyWaveState.totalEnemies)
+                {
+                    GameObject enemy = getPooledObject(poolIndex);
+                    enemy.transform.position = getRandomPoint(transform.position);
+                    enemy.GetComponent<Enemy>().waypointIndex = 0;
+                    enemy.GetComponent<Enemy>().currentHealth = enemy.GetComponent<Enemy>().maxHealth;
+                    enemy.SetActive(true);
+                    enemyWaveState.numSpawned++;
+                }
+                else
+                {
+                    enemyWaveState.completed = true;
+                }
+            }
+        }
     }
 
     public GameObject getPooledObject(int poolIndex)
@@ -75,5 +137,39 @@ public class EnemySpawn : MonoBehaviour
         aux.SetActive(false);
         pools[poolIndex].pooledObjects.Add(aux);
         return aux;
+    }
+
+    public void activateSpawn()
+    {
+        isActive = true;
+        enemyWaves = GameManager.Instance.getCurrentDayWavesSpawn(id);
+        enemyWaveStates.Clear();
+        //initialize enemyWaveStates
+        foreach (EnemyWave enemywave in enemyWaves)
+        {
+            EnemyWaveState state = new EnemyWaveState(enemywave.totalEnemies, 0, false, 0, enemywave.enemyId, enemywave.enemySpawnRate, enemywave.enemyAmountPerSpawn, enemywave.delay, false);
+            enemyWaveStates.Add(state);
+        }
+    }
+
+    public void deactivateSpawn()
+    {
+        isActive = false;
+    }
+
+    public int getPoolIndex(string enemyId)
+    {
+        return pools.IndexOf(pools.Find((x) => x.enemyId == enemyId));
+    }
+
+    private Vector3 getRandomPoint(Vector3 center)
+    {
+        return center + new Vector3((Random.value - 0.5f) * AREA_RANGE, (Random.value - 0.5f) * AREA_RANGE, 0);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        UnityEditor.Handles.color = Color.green;
+        UnityEditor.Handles.DrawWireDisc(transform.position, transform.up, AREA_RANGE);
     }
 }
