90e33033d3848601cab8e8f62d024ee797abc44f
Aram Plaza Bel <arampb84@gmail.com>
2023-02-01 02:31:38 +0100

    Resin+Seed Turrets

--- Assets/Scripts/TurretsBehaviour/ResinBullet.cs
+++ Assets/Scripts/TurretsBehaviour/ResinBullet.cs
@@ -2,26 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SeedBullet : MonoBehaviour
+public class ResinBullet : MonoBehaviour
 {
 
     [SerializeField]
-    private float moveSpeed;
+    private float slowness, duration;
 
-    private Transform target;
+    private float startDuration;
 
     private int damage;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        if(Time.time >= startDuration + duration)
+        {
+            Destroy();
+        }
     }
 
     private void Destroy()
@@ -29,22 +32,26 @@ public class SeedBullet : MonoBehaviour
         gameObject.SetActive(false);
     }
 
-    public void SetTarget(Transform target)
-    {
-        this.target = target;
-    }
-
     public void SetDamage(int bulletDamage)
     {
         damage = bulletDamage;
+    }
+
+    public void SetDuration(float newDuration)
+    {
+        duration = newDuration;
+    }
+
+    private void OnEnable()
+    {
+        startDuration = Time.time;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
-            Destroy();
-            //collision.gameObject.GetComponent<EnemyScript>().Damage();
+            //collision.gameObject.GetComponent<EnemyScript>().Slow() i dmg;
         }
     }
 }
