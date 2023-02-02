37c74c19e4a6a65711e18d9d2a1cbe3a3ac4080b
Dumper23 <carlostubes1@gmail.com>
2023-02-02 18:14:57 +0100

    GameManager DB fix

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -102,99 +102,75 @@ public class GameManager : MonoBehaviour
         {
             case 1:
                 return Database.DAY1_WAVES;
-                break;
-
+                
             case 2:
                 return Database.DAY2_WAVES;
-                break;
 
             case 3:
                 return Database.DAY3_WAVES;
-                break;
 
             case 4:
                 return Database.DAY4_WAVES;
-                break;
 
             case 5:
                 return Database.DAY5_WAVES;
-                break;
 
             case 6:
                 return Database.DAY6_WAVES;
-                break;
 
             case 7:
                 return Database.DAY7_WAVES;
-                break;
 
             case 8:
                 return Database.DAY8_WAVES;
-                break;
 
             case 9:
                 return Database.DAY9_WAVES;
-                break;
 
             case 10:
                 return Database.DAY10_WAVES;
-                break;
 
             case 11:
                 return Database.DAY11_WAVES;
-                break;
 
             case 12:
                 return Database.DAY12_WAVES;
-                break;
 
             case 13:
                 return Database.DAY13_WAVES;
-                break;
 
             case 14:
                 return Database.DAY14_WAVES;
-                break;
 
             case 15:
                 return Database.DAY15_WAVES;
-                break;
 
             case 16:
                 return Database.DAY16_WAVES;
-                break;
 
             case 17:
                 return Database.DAY17_WAVES;
-                break;
 
             case 18:
                 return Database.DAY18_WAVES;
-                break;
 
             case 19:
                 return Database.DAY19_WAVES;
-                break;
 
             case 20:
                 return Database.DAY20_WAVES;
-                break;
 
             case 21:
                 return Database.DAY21_WAVES;
-                break;
 
             case 22:
                 return Database.DAY22_WAVES;
-                break;
 
             case 23:
                 return Database.DAY23_WAVES;
-                break;
 
             default:
                 return new List<EnemyWave>();
-                break;
         }
     }
 
@@ -214,90 +190,174 @@ public class GameManager : MonoBehaviour
 
     public Dictionary<string, float> getTurretInfo(string turretId)
     {
-        int turretLevel;
+        int damageLevel;
+        int speedLevel;
+        int chestLevel;
+        int capacityLevel;
+        int sticknessLevel;
+        int ricochetLevel;
+        int areaLevel;
+        int clusterLevel;
+        int rangeLevel;
+        int extraLevel;
+        int hitsLevel;
+        int piercingLevel;
+        int projectilesLevel;
+        int rayLevel;
+        int stunLevel;
+        int damageStunLevel;
+
         Dictionary<string, float> turretInfo = new Dictionary<string, float>();
         switch (turretId)
         {
             case "MACHINE_SEED":
-                turretLevel = Database.MACHINE_SEED_LVL;
-                turretInfo.Add("capacity", Database.MACHINE_SEED_CAPACITY[turretLevel]);
-                turretInfo.Add("chest", Database.MACHINE_SEED_CHEST[turretLevel]);
-                turretInfo.Add("damage", (int)Database.MACHINE_SEED_DAMAGE[turretLevel]);
-                turretInfo.Add("level", turretLevel);
-                turretInfo.Add("speed", Database.MACHINE_SEED_SPEED[turretLevel]);
+                chestLevel = Database.MACHINE_SEED_CHEST_LVL;
+                capacityLevel = Database.MACHINE_SEED_CAPACITY_LVL;
+                damageLevel = Database.MACHINE_SEED_DAMAGE_LVL;
+                speedLevel = Database.MACHINE_SEED_SPEED_LVL;
+
+                turretInfo.Add("capacity", Database.MACHINE_SEED_CAPACITY[capacityLevel]);
+                turretInfo.Add("chest", Database.MACHINE_SEED_CHEST[chestLevel]);
+                turretInfo.Add("damage", (int)Database.MACHINE_SEED_DAMAGE[damageLevel]);
+                turretInfo.Add("speed", Database.MACHINE_SEED_SPEED[speedLevel]);
+                turretInfo.Add("damageLevel", damageLevel);
+                turretInfo.Add("speedLevel", speedLevel);
+                turretInfo.Add("chestLevel", chestLevel);
+                turretInfo.Add("capacityLevel", capacityLevel);
+
                 return turretInfo;
-                break;
             case "RESIN_SPIT":
-                turretLevel = Database.RESIN_SPIT_LVL;
-                turretInfo.Add("capacity", Database.RESIN_SPIT_CAPACITY[turretLevel]);
-                turretInfo.Add("chest", Database.RESIN_SPIT_CHEST[turretLevel]);
-                turretInfo.Add("damage", Database.RESIN_SPIT_DAMAGE[turretLevel]);
-                turretInfo.Add("level", Database.RESIN_SPIT_LVL);
-                turretInfo.Add("speed", Database.RESIN_SPIT_SPEED[turretLevel]);
-                turretInfo.Add("stick", Database.RESIN_SPIT_STICKNESS[turretLevel]);
+                chestLevel = Database.RESIN_SPIT_CHEST_LVL;
+                capacityLevel = Database.RESIN_SPIT_CAPACITY_LVL;
+                damageLevel = Database.RESIN_SPIT_DAMAGE_LVL;
+                speedLevel = Database.RESIN_SPIT_SPEED_LVL;
+                sticknessLevel = Database.RESIN_SPIT_STICKNESS_LVL;
+
+
+                turretInfo.Add("capacity", Database.RESIN_SPIT_CAPACITY[capacityLevel]);
+                turretInfo.Add("chest", Database.RESIN_SPIT_CHEST[chestLevel]);
+                turretInfo.Add("damage", Database.RESIN_SPIT_DAMAGE[damageLevel]);
+                turretInfo.Add("speed", Database.RESIN_SPIT_SPEED[speedLevel]);
+                turretInfo.Add("stick", Database.RESIN_SPIT_STICKNESS[sticknessLevel]);
+                turretInfo.Add("chestLevel", chestLevel);
+                turretInfo.Add("capacityLevel", capacityLevel);
+                turretInfo.Add("damageLevel", damageLevel);
+                turretInfo.Add("speedLevel", speedLevel);
+                turretInfo.Add("sticknessLevel", sticknessLevel);
                 return turretInfo;
-                break;
             case "S_SEEDNIPER":
-                turretLevel = Database.S_SEEDNIPER_LVL;
-                turretInfo.Add("capacity", Database.S_SEEDNIPER_CAPACITY[turretLevel]);
-                turretInfo.Add("chest", Database.S_SEEDNIPER_CAPACITY[turretLevel]);
-                turretInfo.Add("damage", Database.S_SEEDNIPER_DAMAGE[turretLevel]);
-                turretInfo.Add("level", Database.S_SEEDNIPER_LVL);
-                turretInfo.Add("ricochet", Database.S_SEEDNIPER_RICOCHET[turretLevel]);
-                turretInfo.Add("speed", Database.S_SEEDNIPER_SPEED[turretLevel]);
+                chestLevel = Database.S_SEEDNIPER_CHEST_LVL;
+                capacityLevel = Database.S_SEEDNIPER_CAPACITY_LVL;
+                damageLevel = Database.S_SEEDNIPER_DAMAGE_LVL;
+                ricochetLevel = Database.S_SEEDNIPER_RICOCHET_LVL;
+                speedLevel = Database.S_SEEDNIPER_SPEED_LVL;
+
+                turretInfo.Add("capacity", Database.S_SEEDNIPER_CAPACITY[capacityLevel]);
+                turretInfo.Add("chest", Database.S_SEEDNIPER_CHEST[chestLevel]);
+                turretInfo.Add("damage", Database.S_SEEDNIPER_DAMAGE[damageLevel]);
+                turretInfo.Add("ricochet", Database.S_SEEDNIPER_RICOCHET[ricochetLevel]);
+                turretInfo.Add("speed", Database.S_SEEDNIPER_SPEED[speedLevel]);
+
+                turretInfo.Add("chestLevel", chestLevel);
+                turretInfo.Add("capacityLevel", capacityLevel);
+                turretInfo.Add("damageLevel", damageLevel);
+                turretInfo.Add("ricochetLevel", ricochetLevel);
+                turretInfo.Add("speedLevel", speedLevel);
                 return turretInfo;
-                break;
             case "PINECONE_LAUNCHER":
-                turretLevel = Database.PINECONE_LAUNCHER_LVL;
-                turretInfo.Add("capacity", Database.PINECONE_LAUNCHER_CAPACITY[turretLevel]);
-                turretInfo.Add("chest", Database.PINECONE_LAUNCHER_CHEST[turretLevel]);
-                turretInfo.Add("area", Database.PINECONE_LAUNCHER_AREA[turretLevel]);
-                turretInfo.Add("cluster", Database.PINECONE_LAUNCHER_CLUSTER[turretLevel]);
-                turretInfo.Add("damage", Database.PINECONE_LAUNCHER_DAMAGE[turretLevel]);
-                if(turretLevel > 1)
-                { 
-                    turretInfo.Add("stun", Database.PINECONE_LAUNCHER_DAMAGE_STUN[1]);
-                }
-                turretInfo.Add("level", Database.PINECONE_LAUNCHER_LVL); 
-                turretInfo.Add("range", Database.PINECONE_LAUNCHER_RANGE[turretLevel]); 
-                turretInfo.Add("speed", Database.PINECONE_LAUNCHER_SPEED[turretLevel]); 
+                chestLevel = Database.PINECONE_LAUNCHER_CHEST_LVL;
+                capacityLevel = Database.PINECONE_LAUNCHER_CAPACITY_LVL;
+                areaLevel = Database.PINECONE_LAUNCHER_AREA_LVL;
+                clusterLevel = Database.PINECONE_LAUNCHER_CLUSTER_LVL;
+                damageStunLevel = Database.PINECONE_LAUNCHER_DAMAGE_STUN_LVL;
+                damageLevel = Database.PINECONE_LAUNCHER_DAMAGE_LVL;
+                rangeLevel = Database.PINECONE_LAUNCHER_RANGE_LVL;
+                speedLevel = Database.PINECONE_LAUNCHER_SPEED_LVL;
+
+                turretInfo.Add("capacity", Database.PINECONE_LAUNCHER_CAPACITY[capacityLevel]);
+                turretInfo.Add("chest", Database.PINECONE_LAUNCHER_CHEST[chestLevel]);
+                turretInfo.Add("area", Database.PINECONE_LAUNCHER_AREA[areaLevel]);
+                turretInfo.Add("cluster", Database.PINECONE_LAUNCHER_CLUSTER[clusterLevel]);
+                turretInfo.Add("damage", Database.PINECONE_LAUNCHER_DAMAGE[damageLevel]);
+                turretInfo.Add("stun", Database.PINECONE_LAUNCHER_DAMAGE_STUN[damageStunLevel]);
+                turretInfo.Add("range", Database.PINECONE_LAUNCHER_RANGE[rangeLevel]); 
+                turretInfo.Add("speed", Database.PINECONE_LAUNCHER_SPEED[speedLevel]);
+
+                turretInfo.Add("capacityLevel", capacityLevel);
+                turretInfo.Add("chestLevel", chestLevel);
+                turretInfo.Add("areaLevel", areaLevel);
+                turretInfo.Add("clusterLevel", clusterLevel);
+                turretInfo.Add("damageStunLevel", damageStunLevel);
+                turretInfo.Add("damageLevel", damageLevel);
+                turretInfo.Add("rangeLevel", rangeLevel);
+                turretInfo.Add("speedLevel", speedLevel);
                 return turretInfo;
-                break;
             case "NUT_ROLL":
-                turretLevel = Database.NUT_ROLL_LVL;
-                turretInfo.Add("capacity", Database.NUT_ROLL_CAPACITY[turretLevel]);
-                turretInfo.Add("chest", Database.NUT_ROLL_CHEST[turretLevel]);
-                turretInfo.Add("damage", Database.NUT_ROLL_DAMAGE[turretLevel]);
-                turretInfo.Add("extra", Database.NUT_ROLL_EXTRA[turretLevel]);
-                turretInfo.Add("hits", Database.NUT_ROLL_HITS[turretLevel]);
-                turretInfo.Add("level", Database.NUT_ROLL_LVL);
-                turretInfo.Add("speed", Database.NUT_ROLL_SPEED[turretLevel]);
+                chestLevel = Database.NUT_ROLL_CHEST_LVL;
+                capacityLevel = Database.NUT_ROLL_CAPACITY_LVL;
+                damageLevel = Database.NUT_ROLL_DAMAGE_LVL;
+                extraLevel = Database.NUT_ROLL_EXTRA_LVL;
+                hitsLevel = Database.NUT_ROLL_HITS_LVL;
+                speedLevel = Database.NUT_ROLL_SPEED_LVL;
+
+                turretInfo.Add("capacity", Database.NUT_ROLL_CAPACITY[capacityLevel]);
+                turretInfo.Add("chest", Database.NUT_ROLL_CHEST[chestLevel]);
+                turretInfo.Add("damage", Database.NUT_ROLL_DAMAGE[damageLevel]);
+                turretInfo.Add("extra", Database.NUT_ROLL_EXTRA[extraLevel]);
+                turretInfo.Add("hits", Database.NUT_ROLL_HITS[hitsLevel]);
+                turretInfo.Add("speed", Database.NUT_ROLL_SPEED[speedLevel]);
+
+                turretInfo.Add("chestLevel", chestLevel);
+                turretInfo.Add("capacityLevel", capacityLevel);
+                turretInfo.Add("damageLevel", damageLevel);
+                turretInfo.Add("extraLevel", extraLevel);
+                turretInfo.Add("hitsLevel", hitsLevel);
+                turretInfo.Add("speedLevel", speedLevel);
                 return turretInfo;
-                break;
             case "PORCUTHROW":
-                turretLevel = Database.PORCUTHROW_LVL;
-                turretInfo.Add("capacity", Database.PORCUTHROW_CAPACITY[turretLevel]);
-                turretInfo.Add("chest", Database.PORCUTHROW_CHEST[turretLevel]);
-                turretInfo.Add("level", Database.PORCUTHROW_LVL);
-                turretInfo.Add("piercing", Database.PORCUTHROW_PIERCING[turretLevel]);
-                turretInfo.Add("projectiles", Database.PORCUTHROW_PROJECTILES[turretLevel]);
-                turretInfo.Add("speed", Database.PORCUTHROW_SPEED[turretLevel]);
+                chestLevel = Database.PORCUTHROW_CHEST_LVL;
+                capacityLevel = Database.PORCUTHROW_CAPACITY_LVL;
+                piercingLevel = Database.PORCUTHROW_PIERCING_LVL;
+                projectilesLevel = Database.PORCUTHROW_PROJECTILES_LVL;
+                speedLevel = Database.PORCUTHROW_SPEED_LVL;
+
+                turretInfo.Add("capacity", Database.PORCUTHROW_CAPACITY[capacityLevel]);
+                turretInfo.Add("chest", Database.PORCUTHROW_CHEST[chestLevel]);
+                turretInfo.Add("piercing", Database.PORCUTHROW_PIERCING[piercingLevel]);
+                turretInfo.Add("projectiles", Database.PORCUTHROW_PROJECTILES[projectilesLevel]);
+                turretInfo.Add("speed", Database.PORCUTHROW_SPEED[speedLevel]);
+
+                turretInfo.Add("chestLevel", chestLevel);
+                turretInfo.Add("capacityLevel", capacityLevel);
+                turretInfo.Add("piercingLevel", piercingLevel);
+                turretInfo.Add("projectilesLevel", projectilesLevel);
+                turretInfo.Add("speedLevel", speedLevel);
                 return turretInfo;
-                break;
             case "ELECTRIC_POTATO":
-                turretLevel = Database.ELECTRIC_POTATO_LVL;
-                turretInfo.Add("capacity", Database.ELECTRIC_POTATO_CAPACITY[turretLevel]);
-                turretInfo.Add("chest", Database.ELECTRIC_POTATO_CHEST[turretLevel]);
-                turretInfo.Add("damage", Database.ELECTRIC_POTATO_DAMAGE[turretLevel]);
-                turretInfo.Add("level", Database.ELECTRIC_POTATO_LVL);
-                turretInfo.Add("ray", Database.ELECTRIC_POTATO_RAY[turretLevel]);
-                turretInfo.Add("speed", Database.ELECTRIC_POTATO_SPEED[turretLevel]);
-                turretInfo.Add("stun", Database.ELECTRIC_POTATO_STUN[turretLevel]);
+                chestLevel = Database.ELECTRIC_POTATO_CHEST_LVL;
+                capacityLevel = Database.ELECTRIC_POTATO_CAPACITY_LVL;
+                damageLevel = Database.ELECTRIC_POTATO_DAMAGE_LVL;
+                rayLevel = Database.ELECTRIC_POTATO_RAY_LVL;
+                speedLevel = Database.ELECTRIC_POTATO_SPEED_LVL;
+                stunLevel = Database.ELECTRIC_POTATO_STUN_LVL;
+
+                turretInfo.Add("capacity", Database.ELECTRIC_POTATO_CAPACITY[capacityLevel]);
+                turretInfo.Add("chest", Database.ELECTRIC_POTATO_CHEST[chestLevel]);
+                turretInfo.Add("damage", Database.ELECTRIC_POTATO_DAMAGE[damageLevel]);
+                turretInfo.Add("ray", Database.ELECTRIC_POTATO_RAY[rayLevel]);
+                turretInfo.Add("speed", Database.ELECTRIC_POTATO_SPEED[speedLevel]);
+                turretInfo.Add("stun", Database.ELECTRIC_POTATO_STUN[stunLevel]);
+
+                turretInfo.Add("chestLevel", chestLevel);
+                turretInfo.Add("capacityLevel", capacityLevel);
+                turretInfo.Add("damageLevel", damageLevel);
+                turretInfo.Add("rayLevel", rayLevel);
+                turretInfo.Add("speedLevel", speedLevel);
+                turretInfo.Add("stunLevel", stunLevel);
                 return turretInfo;
-                break;
             default:
+
                 return null;
-                break;
         }
     }
 
