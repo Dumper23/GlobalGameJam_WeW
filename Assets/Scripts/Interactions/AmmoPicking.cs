b5c7015216288209896ff084fc1cfa8840d994b1
Dumper23 <carlostubes1@gmail.com>
2023-02-02 02:24:30 +0100

    Ammo picking system + turret placement

---
+++ Assets/Scripts/Interactions/AmmoPicking.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPicking : IEInteractable
+{
+    public string ammoType;
+
+    public override void EndInteraction()
+    {
+        
+    }
+
+    public override void Interaction(string action = "")
+    {
+        //Player ammo inventory add ammo
+        if (GameManager.Instance.pickUpAmmo(ammoType))
+        {
+            //Player sound allowing action
+            Debug.Log("Ammo taken!");
+        }
+        else
+        {
+            //Player sound denying action
+            Debug.Log("Ammo inventory full");
+        }
+    }
+}
