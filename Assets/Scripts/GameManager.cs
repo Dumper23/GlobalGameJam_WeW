e3722ce1f91b5d152545abfea2d1cc9023ef8cf8
Carles <carlostubes1@gmail.com>
2023-02-05 07:17:46 +0100

    Day light fix

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -72,6 +72,17 @@ public class GameManager : MonoBehaviour
 
     private bool gamePaused = false;
 
+    public UnityEngine.Rendering.Universal.Light2D globalLight;
+    public float intensityDay = 2;
+    public float intensityNight = 0.45f;
+    public Color dayColor;
+    public Color nightColor;
+    private float dayChangeStartTime;
+    private float nightChangeStartTime;
+    public float dayChangeDuration = 4f;
+    public float nightChangeDuration = 4f;
+    private bool changeLight;
+
     #region Constants
 
     public static string ANT_ID = "ant";
@@ -109,6 +120,8 @@ public class GameManager : MonoBehaviour
         floorList.Add(GameObject.Find("BOTTOM"));
         floorList.Add(GameObject.Find("Blue_Floor"));
         floorList.Add(GameObject.Find("TOP"));
+
+        changeDayState();
     }
 
     #region getters & setters
@@ -223,6 +236,7 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
+        DayLight();
         if (!alreadyInteracted)
         {
             player.liftDelayCircle.gameObject.SetActive(false);
@@ -231,6 +245,47 @@ public class GameManager : MonoBehaviour
         {
             player.liftDelayCircle.gameObject.SetActive(true);
             player.liftDelayCircle.fillAmount -= Time.deltaTime;
+        }
+    }
+
+    public void DayLight()
+    {
+        if (changeLight)
+        {
+            if (isDay)
+            {
+                if (Time.time >= dayChangeStartTime + dayChangeDuration)
+                {
+                    changeLight = false;
+                }
+                else
+                {
+                    globalLight.intensity = LerpValues.Lerp(intensityNight, intensityDay, dayChangeStartTime, dayChangeDuration);
+
+                    float r = LerpValues.Lerp(nightColor.r, dayColor.r, dayChangeStartTime, dayChangeDuration);
+                    float g = LerpValues.Lerp(nightColor.g, dayColor.g, dayChangeStartTime, dayChangeDuration);
+                    float b = LerpValues.Lerp(nightColor.b, dayColor.b, dayChangeStartTime, dayChangeDuration);
+
+                    globalLight.color = new Color(r, g, b);
+                }
+            }
+            else
+            {
+                if (Time.time >= nightChangeStartTime + nightChangeDuration)
+                {
+                    changeLight = false;
+                }
+                else
+                {
+                    globalLight.intensity = LerpValues.Lerp(intensityDay, intensityNight, nightChangeStartTime, nightChangeDuration);
+
+                    float r = LerpValues.Lerp(dayColor.r, nightColor.r, nightChangeStartTime, nightChangeDuration);
+                    float g = LerpValues.Lerp(dayColor.g, nightColor.g, nightChangeStartTime, nightChangeDuration);
+                    float b = LerpValues.Lerp(dayColor.b, nightColor.b, nightChangeStartTime, nightChangeDuration);
+
+                    globalLight.color = new Color(r, g, b);
+                }
+            }
         }
     }
 
@@ -871,8 +926,10 @@ public class GameManager : MonoBehaviour
         infoUpdater.ChangeDay();
         player.updateTurretPlacementMenu();
         isDay = !isDay;
+        changeLight = true;
         if (isDay)
         {
+            dayChangeStartTime = Time.time;
             //show animation
             mainCam.GetComponent<CameraFollow>().blurCamera();
             this.hud.transform.Find("Background").gameObject.SetActive(true);
@@ -881,11 +938,11 @@ public class GameManager : MonoBehaviour
             Invoke("removeDayNightAnimation", 5);
 
             currentDay++;
-
             Invoke("activateEnemySpawns", 6);
         }
         else
         {
+            nightChangeStartTime = Time.time;
             //show animation
             mainCam.GetComponent<CameraFollow>().blurCamera();
             this.hud.transform.Find("Background").gameObject.SetActive(true);
